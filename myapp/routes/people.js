8f71e75ade0acdac73f67539fe9153eb9db4e81f
mitchg1983 <mitchg83@Geoffreys-MacBook-Pro.local>
2022-03-28 14:57:04 -0400

    Add get one person by id route.

--- myapp/routes/people.js
+++ myapp/routes/people.js
@@ -5,10 +5,12 @@ const {
     getAllPeople,
     createPerson,
     updatePerson,
+    getOnePerson
 } = require("./people/controller/peopleController");
 
 router.get("/get-all-people", getAllPeople);
 router.post("/create-person", createPerson);
 router.put("/update-person", updatePerson);
+router.get("/get-one-person/:id", getOnePerson);
 
 module.exports = router;
--- myapp/routes/people/controller/peopleController.js
+++ myapp/routes/people/controller/peopleController.js
@@ -2,7 +2,7 @@ const People = require("../model/peopleModel");
 const errorHandler = require("../utils/errorHandler");
 
 const getAllPeople = async (req, res) => {
-    console.log("Beginning getAllPeople");
+  console.log("Beginning getAllPeople");
   try {
     let allPeople = await People.find();
     res.status(200).json({
@@ -14,51 +14,68 @@ const getAllPeople = async (req, res) => {
 };
 
 const createPerson = async (req, res) => {
-    try {
-      console.log(req.body);
-      const { name, age, email, favoriteMovies } = req.body;
-      const newPerson = new People({
-        name: name,
-        age: age,
-        email: email,
-        favoriteMovies: favoriteMovies,
-      });
-      const savedPerson = await newPerson.save();
-      res.status(200).json({
-        message: "New person was saved",
-        payload: savedPerson,
-      });
-    } catch (error) {
-      if (error.code === 11000) {
-        return res.status(500).json({
-          message: error,
-          error: `duplicate?`,
-        });
-      }
-      res.json({
-        message: "There is an error......",
-        error: error.errors,
+  try {
+    console.log(req.body);
+    const { name, age, email, favoriteMovies } = req.body;
+    const newPerson = new People({
+      name: name,
+      age: age,
+      email: email,
+      favoriteMovies: favoriteMovies,
+    });
+    const savedPerson = await newPerson.save();
+    res.status(200).json({
+      message: "New person was saved",
+      payload: savedPerson,
+    });
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.status(500).json({
+        message: error,
+        error: `duplicate?`,
       });
     }
-  };
-
+    res.json({
+      message: "There is an error......",
+      error: error.errors,
+    });
+  }
+};
 
 const updatePerson = async (req, res) => {
+  try {
+    const updatedPerson = await People.findOneAndUpdate(
+      { email: req.body.email },
+      req.body,
+      { new: true }
+    );
+    res
+      .status(200)
+      .json({
+        message: "This person has been updated.",
+        payload: updatedPerson,
+      });
+  } catch (error) {
+    res.status(500).json({ error: errorHandler(error) });
+  }
+};
+
+const getOnePerson = async (req, res) => {
     try {
-        const updatedPerson = await People.findOneAndUpdate(
-            { email: req.body.email },
-            req.body,
-            { new: true }
-        );
-        res.status(200).json({ message: "This person has been updated.", payload: updatedPerson });
+        const { id } = req.params;
+        let foundPerson = await People.findById(id);
+        res.status(200).json({ 
+            message: "Person found.",
+            payload: foundPerson,
+        })
     } catch (error) {
-        res.status(500).json({ error: errorHandler(error) })
+        res.status(500).json(error);
     }
 }
-  
 
 module.exports = {
-    getAllPeople,
-    createPerson,
-    updatePerson,
+  getAllPeople,
+  createPerson,
+  updatePerson,
+  getOnePerson
 };
